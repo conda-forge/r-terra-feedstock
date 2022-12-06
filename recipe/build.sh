569742cc945d6ddf45efff417b29b61301bbe17b
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-12-06 20:21:11 +0100

    Revert automatic build.sh changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 export DISABLE_AUTOBREW=1
+
+# workarond conda-forge/r-base-feedstock#163
+export PKG_CPPFLAGS="-DHAVE_WORKING_LOG1P"
+
+sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
+sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
