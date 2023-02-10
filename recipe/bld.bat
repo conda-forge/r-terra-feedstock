fda152e756905035bcde3965e5a7cd2953f60567
Mervin Fansler <mmfansler@gmail.com>
2023-02-10 12:40:12 -0500

    run configure

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
 set "LIBRARY_PREFIX_CYG=%LIBRARY_PREFIX:\=/%"
 set "LIBRARY_PREFIX_CYG=/%LIBRARY_PREFIX_CYG::=%"
- 
+
+:: rebuild Makevars.win from scratch
+bash ./configure
+bash -c "mv src/Makevars src/Makevars.win"
+
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
