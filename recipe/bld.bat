d0e950e2c0327b10b296653cbdab93ab59b5fb5b
Mervin Fansler <mmfansler@gmail.com>
2023-02-10 13:56:31 -0500

    drop deps

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,5 @@
 set "LIBRARY_PREFIX_CYG=%LIBRARY_PREFIX:\=/%"
 set "LIBRARY_PREFIX_CYG=/%LIBRARY_PREFIX_CYG::=%"
 
-:: rebuild Makevars.win from scratch
-autoreconf --force
-bash ./configure
-bash -c "mv src/Makevars src/Makevars.win"
-
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
