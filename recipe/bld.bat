22b7331931b79896dce28840426f9231eb01b1b1
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-07-11 09:49:27 +0200

    debug

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,9 @@
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig"
+
+REM begin debug (why pkg-config fails)
+pkg-config --version
+pkg-config --libs gdal geos proj
+REM end debug
+
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
