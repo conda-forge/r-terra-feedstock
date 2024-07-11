57baf202d1df8ef70f099cc78889c2fe5f249854
Mervin Fansler <mervin.fansler@bric.ku.dk>
2024-07-11 19:44:37 +0200

    debug - check path contents

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig"
 
 REM begin debug (why pkg-config fails)
+echo %PKG_CONFIG_PATH%
+dir /a /b "%PKG_CONFIG_PATH%"
 pkg-config --version
 pkg-config --libs gdal geos proj
 REM end debug
