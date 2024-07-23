3ab4500b39635e792cc7b9697d6c8acc9f3cc492
Mervin Fansler <mmfansler@gmail.com>
2024-07-23 23:26:58 +0200

    emit .pc contents

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,12 @@
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig"
 
 REM begin debug (why pkg-config fails)
+@echo on
 echo %PKG_CONFIG_PATH%
 dir /a /b "%PKG_CONFIG_PATH%"
+type "%PKG_CONFIG_PATH%\gdal.pc"
+type "%PKG_CONFIG_PATH%\geos.pc"
+type "%PKG_CONFIG_PATH%\proj.pc"
 pkg-config --version
 pkg-config --libs gdal geos proj
 REM end debug
